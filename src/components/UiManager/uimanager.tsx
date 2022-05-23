50d0657e03be68e71b4827970584239903cc9b61
Manuel Cascallar <cascallarmanuel@gmail.com>
2022-05-23 13:29:22 -0300

    UI Start

--- src/components/UiManager/uimanager.tsx
+++ src/components/UiManager/uimanager.tsx
@@ -2,5 +2,16 @@ import React from 'react';
 import './styles.css';
 
 export function UIManager(): React.ReactElement {
-  return <div id="uimanager" />;
+  return (
+    <div id="uimanager">
+      <div className="bottomBar">
+        <button type="button" className="barButton" onClick={() => console.log('BUILD BASE')}>
+          Base
+        </button>
+        <button type="button" className="barButton" onClick={() => console.log('BUILD BASE')}>
+          Barracks
+        </button>
+      </div>
+    </div>
+  );
 }
