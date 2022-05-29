b257535e3d63ead368302ff835bd1cd29baafb03
Manuel Cascallar <cascallarmanuel@gmail.com>
2022-05-29 15:47:03 -0300

    We'll see pawns later

--- src/components/GameObjects/Base/base.tsx
+++ src/components/GameObjects/Base/base.tsx
@@ -1,21 +1,12 @@
 import { MeshProps } from '@react-three/fiber';
 import React from 'react';
-import { Vector3 } from 'three';
-import { Pawn } from '../Pawn';
 
 /** Base for spawning pawns */
 export const Base = (props: MeshProps): React.ReactElement => (
   <mesh
-    //   onPointerDown={() => console.log("mouse down")}
-    //   onPointerUp={() => console.log("mouse Up")}
     {...props}
   >
     <boxGeometry args={[1, 1.5, 1]} />
     <meshStandardMaterial color="lemonchiffon" />
-    <Pawn
-      position={[0, 0.5, 0]}
-      inputDirection={new Vector3(0.05, 0, 0)}
-      maxDistance={15}
-    />
   </mesh>
 );
